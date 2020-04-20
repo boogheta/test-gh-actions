d2161147efb78f9f35ce1750f381411d68cc0a00
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2020-04-20 17:30:11 +0200

    add git commit to update script

--- update.sh
+++ update.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+echo "Running update.sh"
+
+echo "Updating data"
 date +%s > data
+cat data
+
+echo "Running git add"
+git add data
+
+echo "Running git commit"
+git commit -m "autoupdate"
+
+echo "Running git push"
+git push
